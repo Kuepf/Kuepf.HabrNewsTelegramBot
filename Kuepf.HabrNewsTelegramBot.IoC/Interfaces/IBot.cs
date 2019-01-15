d58803565e557c8d1f16ae9977ef58f972661469
Daniel <daniel.alexandrov.s@gmail.com>
2019-01-15 03:01:04 +0200

    Initial start of architecture refactoring

---
+++ Kuepf.HabrNewsTelegramBot.IoC/Interfaces/IBot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Kuepf.HabrNewsTelegramBot.IoC.Interfaces
+{
+    public interface IBot
+    {
+        Task<TelegramBotClient> GetBotClientAsync();
+    }
+}
---
+++ Kuepf.HabrNewsTelegramBot.IoC/Interfaces/IHabrScraper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuepf.HabrNewsTelegramBot.IoC.Interfaces
+{
+    public interface IHabrScraper
+    {
+        Task<List<string>> GetPosts();
+    }
+}
