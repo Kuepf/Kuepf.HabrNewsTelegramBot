df8677d742c4acfff88da05bdc5692c3a7f7ae86
Kuepf <madmanpasha@gmail.com>
2019-01-15 02:21:54 +0200

    Added scraper service

---
+++ Kuepf.HabrNewsTelegramBot.API/Services/IHabrScraper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kuepf.HabrNewsTelegramBot.API.Services
+{
+    public interface IHabrScraper
+    {
+        Task<List<string>> GetPosts();
+    }
+}
